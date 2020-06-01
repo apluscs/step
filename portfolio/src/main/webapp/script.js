a3959297e3c27a7d2f105ab69a33225ce922e08b
Claire Y <the.claire.yang@gmail.com>
2020-06-01 12:32:56 -0400

    Added random greeting through RandomGreetingServlet.java to index.html of portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,3 +11,13 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+window.onload = () => { 
+  fetch('/random-greeting').then((response) => {
+    console.log("got the response")
+    return response.text();
+  }).then((greeting) => {
+    console.log('Adding greeting to dom: ' + greeting);
+    document.getElementById('greeting-container').innerText = greeting;
+  })
+}
