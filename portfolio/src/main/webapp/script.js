48b65ce6d7775096d80f982537e2f284619df9e6
Claire <the.claire.yang@gmail.com>
2020-06-08 10:52:34 -0400

    added front end login/logout functionality to navbar

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -20,7 +20,32 @@ function loadPage() {
 function renderAuthentication(){
   fetch('/authenticate').then((response) => response.json()).then((response) => {
     debugLog(response);
+    const navbarList = document.getElementById('navbar-list');
+    if(response.isUserLoggedIn){
+      navbarList.appendChild(createAuthenticatedStatusElement(response.logoutUrl, "Logout"));
+    } else {
+      navbarList.appendChild(createAuthenticatedStatusElement(response.loginUrl, "Login"));
+    }
   });
+}
+
+function createAuthenticatedStatusElement(href, text) {
+  const listElement = document.createElement("li"); 
+  listElement.classList.add("nav-item");
+  listElement.classList.add("active");
+
+  const link = document.createElement("a"); 
+  link.classList.add("nav-link");
+  link.setAttribute("href", href);
+  link.innerHTML = text;
+
+  const span = document.createElement("span");
+  span.classList.add("sr-only");
+  span.innerHTML = "(current)";
+  link.appendChild(span); 
+
+  listElement.appendChild(link);
+  return listElement;
 }
 
 function renderRandomGreeting(){
