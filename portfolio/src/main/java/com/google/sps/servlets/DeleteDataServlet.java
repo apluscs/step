d7848cb9a116507bf428c35a7b97a65fa17b91c2
Claire Y <the.claire.yang@gmail.com>
2020-06-11 20:37:36 +0000

    removed extra lines

--- portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
@@ -29,8 +29,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
 /** Servlet that handles comments data */
 @WebServlet("/delete-data")
 public class DeleteDataServlet extends HttpServlet {
