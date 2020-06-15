e08c1b0e1cbf5e46e28206ca20bfd72827b4ed97
Claire Y <the.claire.yang@gmail.com>
2020-06-15 19:29:57 +0000

    added documentation, renamed variables, made removeIrrelevantEvents faster

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -23,11 +23,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/** Class that lists possible meeting times based on meeting information it takes in. */
 public final class FindMeetingQuery {
-  public static final int START_OF_DAY = TimeRange.getTimeInMinutes(0, 0);
-  public static final int END_OF_DAY = TimeRange.getTimeInMinutes(23, 59);
+  private static final int END_OF_DAY = TimeRange.getTimeInMinutes(23, 59);
 
-  public static final Comparator<Event> ORDER_BY_START =
+  private static final Comparator<Event> ORDER_BY_START =
       new Comparator<Event>() {
         @Override
         public int compare(Event a, Event b) {
@@ -35,62 +35,86 @@ public final class FindMeetingQuery {
         }
       };
 
+  /**
+   * Returns a list of time periods in which the meeting, specified by request, could happen.
+   *
+   * @param eventsCollection the events we know about
+   * @param request information about the meeting, including attendees, optional attendees, and how
+   *     long it needs to be
+   */
   public Collection<TimeRange> query(Collection<Event> eventsCollection, MeetingRequest request) {
     HashSet<String> requestAttendees = new HashSet<String>(request.getAttendees());
     ArrayList<Event> events =
         removeIrrelevantEvents(requestAttendees, new ArrayList<Event>(eventsCollection));
-    List<TimeRange> res = new ArrayList<TimeRange>();
+    List<TimeRange> possibleMeetingTimes = new ArrayList<TimeRange>();
     if (events.isEmpty()) {
-      addIfValid(TimeRange.fromStartEnd(0, END_OF_DAY, true), res, request.getDuration());
-      return res;
+      addIfLongEnough(
+          TimeRange.fromStartEnd(0, END_OF_DAY, true), possibleMeetingTimes, request.getDuration());
+      return possibleMeetingTimes;
     }
-
     Collections.sort(events, ORDER_BY_START);
 
     // Add first gap.
-    addIfValid(
+    addIfLongEnough(
         TimeRange.fromStartEnd(0, events.get(0).getWhen().start(), false),
-        res,
+        possibleMeetingTimes,
         request.getDuration());
     int end = events.get(0).getWhen().end();
     for (Event event : events) {
       // event can be merged with current time range
       if (event.getWhen().start() <= end) {
         end = Math.max(end, event.getWhen().end());
-      } else {
-        // Add the time range we were tracking, start a new one from event.
-        addIfValid(
-            TimeRange.fromStartEnd(end, event.getWhen().start(), false),
-            res,
-            request.getDuration());
-        end = event.getWhen().end();
+        continue;
       }
+      // Add the time range we were tracking, start a new one from event.
+      addIfLongEnough(
+          TimeRange.fromStartEnd(end, event.getWhen().start(), false),
+          possibleMeetingTimes,
+          request.getDuration());
+      end = event.getWhen().end();
     }
 
     // Add the last one we were tracking.
-    addIfValid(TimeRange.fromStartEnd(end, END_OF_DAY, true), res, request.getDuration());
-    return res;
+    addIfLongEnough(
+        TimeRange.fromStartEnd(end, END_OF_DAY, true), possibleMeetingTimes, request.getDuration());
+    return possibleMeetingTimes;
   }
 
-  void addIfValid(TimeRange range, List<TimeRange> ranges, long duration) {
-    if (range.duration() >= duration) {
+  /**
+   * Adds range to ranges if it is long enough to fit in a meeting.
+   *
+   * @param range the range being considered
+   * @param ranges the list of ranges >= meetingDuration
+   * @param meetingDuration the duration of meeting to be scheduled
+   */
+  private static void addIfLongEnough(
+      TimeRange range, List<TimeRange> ranges, long meetingDuration) {
+    if (range.duration() >= meetingDuration) {
       ranges.add(range);
     }
   }
 
-  ArrayList<Event> removeIrrelevantEvents(
-      HashSet<String> requestAttendees, ArrayList<Event> events) {
-    ArrayList<Event> res = new ArrayList<Event>();
+  /**
+   * Returns only those events that are attended by at least one attendee that is attending the
+   * meeting we are trying to schedule. More intuitively, an event is "relevant" if it is attended
+   * by at least one "relevant" attendee.
+   *
+   * @param requestAttendees the set of attendees attending the meeting ("relevant" people)
+   */
+  private static ArrayList<Event> removeIrrelevantEvents(
+      HashSet<String> relevantAttendees, ArrayList<Event> events) {
+    ArrayList<Event> relevantEvents = new ArrayList<Event>();
     for (Event event : events) {
-      Set<String> eventAttendees = new HashSet<String>(event.getAttendees());
-
-      // This event has at least one relevant attendee, must be recognized.
-      Set<String> intersection = new HashSet<String>(eventAttendees);
-      intersection.retainAll(requestAttendees);
-      if (!intersection.isEmpty()) {
-        res.add(event);
+      boolean isRelevant = false;
+      for (String person : event.getAttendees()) {
+        if (relevantAttendees.contains(person)) {
+          isRelevant = true;
+        }
+      }
+      if (isRelevant) {
+        relevantEvents.add(event);
       }
     }
-    return res;
+    return relevantEvents;
   }
 }
