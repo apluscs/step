c26debb53ec7d2fb45d4826a63af4a7256a24f7d
Claire Y <the.claire.yang@gmail.com>
2020-06-15 13:52:33 +0000

    sorted events by start time

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,36 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+  public static final int START_OF_DAY = TimeRange.getTimeInMinutes(0, 0);
+  public static final int END_OF_DAY = TimeRange.getTimeInMinutes(23, 59);
+  
+  public static final Comparator<Event> ORDER_BY_START = new Comparator<Event>() {
+    @Override
+    public int compare(Event a, Event b) {
+      return Long.compare(a.getWhen().start(), b.getWhen().start());
+    }
+  };
+  
+  public Collection<TimeRange> query(Collection<Event> events_collection, MeetingRequest request) {
+    List<Event> events = new ArrayList<Event>(events_collection);
+    for(Event e:events){
+      System.out.println(e.getWhen().start() + " to " + e.getWhen().end());
+    }
+    System.out.println("---------------");
+    Collections.sort(events, ORDER_BY_START);
+    for(Event e:events){
+      System.out.println(e.getWhen().start() + " to " + e.getWhen().end());
+    }
+    System.out.println();
+    List<TimeRange> res = new ArrayList<TimeRange>();
+    return res;
   }
 }
